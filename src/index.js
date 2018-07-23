51bed2dccd7d1d972aff3fec550429aa0dc6ba9e
Steve Mason <stevem@brandwatch.com>
2018-07-23 13:13:01 +0100

    Upgraded eslint and rules. Fixed errors

--- src/index.js
+++ src/index.js
@@ -9,20 +9,19 @@ let healthChecks = [];
 
 const prometheusContentType = 'text/plain; version=0.0.4';
 
-const doCheck = ({ name, check }) =>
-  new Promise((resolve) => {
-    // Pass in the [ok, warning, critical, unknown] callbacks. i.e. just resolve the promise.
-    check(
-      () => { resolve({ name, value: 0 }); },
-      () => { resolve({ name, value: 1 }); },
-      () => { resolve({ name, value: 2 }); },
-      () => { resolve({ name, value: 3 }); }
-    );
-  });
+const doCheck = ({ name, check }) => new Promise((resolve) => {
+  // Pass in the [ok, warning, critical, unknown] callbacks. i.e. just resolve the promise.
+  check(
+    () => { resolve({ name, value: 0 }); },
+    () => { resolve({ name, value: 1 }); },
+    () => { resolve({ name, value: 2 }); },
+    () => { resolve({ name, value: 3 }); }
+  );
+});
 
-const doReadinessCheck = ({ check }) =>
-  // Pass four callbacks so that health checks can be directly re-used for readiness checks
-  new Promise((resolve, reject) => check(resolve, reject, reject, reject));
+const doReadinessCheck = ({ check }) => new Promise((resolve, reject) => {
+  check(resolve, reject, reject, reject);
+});
 
 const toPrometheusFormat = (str, { name, value }) => `${str}${name} ${value}\n`;
 
@@ -38,11 +37,11 @@ const getMiddleware = () => (req, res, next) => {
       return Promise.resolve().then(() => res.sendStatus(200));
     }
 
-    const promisedChecks = readinessChecks.map((check) =>
-      new Promise((resolve, reject) => doReadinessCheck(check)
+    const promisedChecks = readinessChecks.map((check) => new Promise((resolve, reject) => {
+      doReadinessCheck(check)
         .then(resolve)
-        .catch(reject))
-    );
+        .catch(reject);
+    }));
 
     return Promise.all(promisedChecks)
       .then(() => res.sendStatus(200))
@@ -50,41 +49,38 @@ const getMiddleware = () => (req, res, next) => {
   }
 
   // Run the checks and return in prometheus formats
-  else if (req.path === '/checkz') {
+  if (req.path === '/checkz') {
     res.set('Content-Type', prometheusContentType);
 
     if (healthChecks.length < 1) {
       return Promise.resolve().then(() => res.sendStatus(404));
     }
 
-    const promisedChecks = healthChecks.map((check) =>
-      new Promise((resolve, reject) => doCheck(check)
+    const promisedChecks = healthChecks.map((check) => new Promise((resolve, reject) => {
+      doCheck(check)
         .then(resolve)
-        .catch(reject)
-      )
-    );
+        .catch(reject);
+    }));
 
     return Promise.all(promisedChecks)
-      .then((check) =>
-        res.send(
-          check.reduce(toPrometheusFormat, ''))
-      );
+      .then((check) => res.send(
+        check.reduce(toPrometheusFormat, '')
+      ));
   }
 
   // Run the checks and error if the app is not healthy
-  else if (req.path === '/livez') {
+  if (req.path === '/livez') {
     res.set('Content-Type', prometheusContentType);
 
     if (healthChecks.length < 1) {
       return Promise.resolve().then(() => res.sendStatus(200));
     }
 
-    const promisedChecks = healthChecks.map((check) =>
-      new Promise((resolve, reject) => doCheck(check)
+    const promisedChecks = healthChecks.map((check) => new Promise((resolve, reject) => {
+      doCheck(check)
         .then(resolve)
-        .catch(reject)
-      )
-    );
+        .catch(reject);
+    }));
 
     return Promise.all(promisedChecks)
       .then((statuses) => {
@@ -99,7 +95,7 @@ const getMiddleware = () => (req, res, next) => {
   }
 
   // Send any prometheus metrics specified
-  else if (req.path === '/metricz') {
+  if (req.path === '/metricz') {
     res.set('Content-Type', prometheusContentType);
 
     if (!metrics) {
