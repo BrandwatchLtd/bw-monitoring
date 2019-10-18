fb244fef4bd0ce8e0fdbc357a38e264526a1f88a
Graham Scott <grahams@brandwatch.com>
2019-10-18 13:01:59 +0100

    chore: eslint autofix

--- src/index.js
+++ src/index.js
@@ -9,115 +9,125 @@ let healthChecks = [];
 
 const prometheusContentType = 'text/plain; version=0.0.4';
 
-const doCheck = ({ name, check }) => new Promise((resolve) => {
-  // Pass in the [ok, warning, critical, unknown] callbacks. i.e. just resolve the promise.
-  check(
-    () => { resolve({ name, value: 0 }); },
-    () => { resolve({ name, value: 1 }); },
-    () => { resolve({ name, value: 2 }); },
-    () => { resolve({ name, value: 3 }); }
-  );
+const doCheck = ({name, check}) => new Promise((resolve) => {
+    // Pass in the [ok, warning, critical, unknown] callbacks. i.e. just resolve the promise.
+    check(
+        () => {
+            resolve({name, value: 0});
+        },
+        () => {
+            resolve({name, value: 1});
+        },
+        () => {
+            resolve({name, value: 2});
+        },
+        () => {
+            resolve({name, value: 3});
+        }
+    );
 });
 
-const doReadinessCheck = ({ check }) => new Promise((resolve, reject) => {
-  check(resolve, reject, reject, reject);
+const doReadinessCheck = ({check}) => new Promise((resolve, reject) => {
+    check(resolve, reject, reject, reject);
 });
 
-const toPrometheusFormat = (str, { name, value }) => `${str}${name} ${value}\n`;
+const toPrometheusFormat = (str, {name, value}) => `${str}${name} ${value}\n`;
 
 const addCheck = (check) => healthChecks.push(check);
 const addReadinessCheck = (check) => readinessChecks.push(check);
-const addMetrics = (m) => { metrics = m; };
+const addMetrics = (m) => {
+    metrics = m;
+};
 
 const getMiddleware = () => (req, res, next) => {
-  if (req.path === '/healthz') {
-    res.set('Content-Type', prometheusContentType);
+    if (req.path === '/healthz') {
+        res.set('Content-Type', prometheusContentType);
 
-    if (readinessChecks.length < 1) {
-      return Promise.resolve().then(() => res.sendStatus(200));
-    }
-
-    const promisedChecks = readinessChecks.map((check) => new Promise((resolve, reject) => {
-      doReadinessCheck(check)
-        .then(resolve)
-        .catch(reject);
-    }));
-
-    return Promise.all(promisedChecks)
-      .then(() => res.sendStatus(200))
-      .catch(() => res.sendStatus(500));
-  }
-
-  // Run the checks and return in prometheus formats
-  if (req.path === '/checkz') {
-    res.set('Content-Type', prometheusContentType);
-
-    if (healthChecks.length < 1) {
-      return Promise.resolve().then(() => res.sendStatus(404));
-    }
-
-    const promisedChecks = healthChecks.map((check) => new Promise((resolve, reject) => {
-      doCheck(check)
-        .then(resolve)
-        .catch(reject);
-    }));
-
-    return Promise.all(promisedChecks)
-      .then((check) => res.send(
-        check.reduce(toPrometheusFormat, '')
-      ));
-  }
-
-  // Run the checks and error if the app is not healthy
-  if (req.path === '/livez') {
-    res.set('Content-Type', prometheusContentType);
-
-    if (healthChecks.length < 1) {
-      return Promise.resolve().then(() => res.sendStatus(200));
-    }
-
-    const promisedChecks = healthChecks.map((check) => new Promise((resolve, reject) => {
-      doCheck(check)
-        .then(resolve)
-        .catch(reject);
-    }));
-
-    return Promise.all(promisedChecks)
-      .then((statuses) => {
-        const badStatuses = statuses.filter(({ value }) => value !== 0);
-        if (badStatuses.length === 0) {
-          res.status(200);
-        } else {
-          res.status(500);
+        if (readinessChecks.length < 1) {
+            return Promise.resolve().then(() => res.sendStatus(200));
         }
-        res.send(statuses.reduce(toPrometheusFormat, ''));
-      });
-  }
 
-  // Send any prometheus metrics specified
-  if (req.path === '/metricz') {
-    res.set('Content-Type', prometheusContentType);
+        const promisedChecks = readinessChecks.map((check) => new Promise((resolve, reject) => {
+            doReadinessCheck(check)
+                .then(resolve)
+                .catch(reject);
+        }));
 
-    if (!metrics) {
-      return Promise.resolve().then(() => res.sendStatus(404));
+        return Promise.all(promisedChecks)
+            .then(() => res.sendStatus(200))
+            .catch(() => res.sendStatus(500));
     }
 
-    return Promise.resolve().then(() => res.send(metrics()));
-  }
+    // Run the checks and return in prometheus formats
+    if (req.path === '/checkz') {
+        res.set('Content-Type', prometheusContentType);
 
-  return next();
+        if (healthChecks.length < 1) {
+            return Promise.resolve().then(() => res.sendStatus(404));
+        }
+
+        const promisedChecks = healthChecks.map((check) => new Promise((resolve, reject) => {
+            doCheck(check)
+                .then(resolve)
+                .catch(reject);
+        }));
+
+        return Promise.all(promisedChecks)
+            .then((check) => res.send(
+                check.reduce(toPrometheusFormat, '')
+            ));
+    }
+
+    // Run the checks and error if the app is not healthy
+    if (req.path === '/livez') {
+        res.set('Content-Type', prometheusContentType);
+
+        if (healthChecks.length < 1) {
+            return Promise.resolve().then(() => res.sendStatus(200));
+        }
+
+        const promisedChecks = healthChecks.map((check) => new Promise((resolve, reject) => {
+            doCheck(check)
+                .then(resolve)
+                .catch(reject);
+        }));
+
+        return Promise.all(promisedChecks)
+            .then((statuses) => {
+                const badStatuses = statuses.filter(({value}) => value !== 0);
+                if (badStatuses.length === 0) {
+                    res.status(200);
+                } else {
+                    res.status(500);
+                }
+                res.send(statuses.reduce(toPrometheusFormat, ''));
+            });
+    }
+
+    // Send any prometheus metrics specified
+    if (req.path === '/metricz') {
+        res.set('Content-Type', prometheusContentType);
+
+        if (!metrics) {
+            return Promise.resolve().then(() => res.sendStatus(404));
+        }
+
+        return Promise.resolve().then(() => res.send(metrics()));
+    }
+
+    return next();
 };
 
 const reset = () => {
-  metrics = undefined;
-  readinessChecks = [];
-  healthChecks = [];
+    metrics = undefined;
+    readinessChecks = [];
+    healthChecks = [];
 };
 
 module.exports = {
-  addCheck,
-  addReadinessCheck,
-  addMetrics,
-  getMiddleware,
-  reset,
+    addCheck,
+    addReadinessCheck,
+    addMetrics,
+    getMiddleware,
+    reset
 };
--- test/index.js
+++ test/index.js
@@ -4,210 +4,210 @@ const sinon = require('sinon');
 const mon = require('../src/index.js');
 
 describe('bw-monitoring', () => {
-  let req;
-  let res;
-  let next;
+    let req;
+    let res;
+    let next;
 
-  beforeEach(() => {
-    req = {};
-    res = {
-      set: sinon.spy(),
-      sendStatus: sinon.spy(),
-      status: sinon.spy(),
-      send: sinon.spy(),
-    };
-    next = sinon.spy();
-  });
-
-  afterEach(() => {
-    mon.reset();
-  });
-
-  it('ignores unrequired url paths', () => {
-    req.path = '/your-app';
-    const mid = mon.getMiddleware();
-    mid(req, res, next);
-    assert(next.called);
-  });
-
-  describe('/healthz endpoint', () => {
     beforeEach(() => {
-      req.path = '/healthz';
+        req = {};
+        res = {
+            set: sinon.spy(),
+            sendStatus: sinon.spy(),
+            status: sinon.spy(),
+            send: sinon.spy()
+        };
+        next = sinon.spy();
     });
 
-    it('should return 200 by default', (done) => {
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.sendStatus.calledWith(200));
-          done();
+    afterEach(() => {
+        mon.reset();
+    });
+
+    it('ignores unrequired url paths', () => {
+        req.path = '/your-app';
+        const mid = mon.getMiddleware();
+        mid(req, res, next);
+        assert(next.called);
+    });
+
+    describe('/healthz endpoint', () => {
+        beforeEach(() => {
+            req.path = '/healthz';
+        });
+
+        it('should return 200 by default', (done) => {
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.sendStatus.calledWith(200));
+                    done();
+                });
+        });
+
+        it('should return 200 if all checks succeed', (done) => {
+            mon.addReadinessCheck({name: 'bob', check: (ok) => ok()});
+            mon.addReadinessCheck({name: 'bob2', check: (ok) => ok()});
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.sendStatus.calledWith(200));
+                    done();
+                });
+        });
+
+        it('should return 500 if any check fails', (done) => {
+            mon.addReadinessCheck({name: 'bob', check: (ok) => ok()});
+            mon.addReadinessCheck({name: 'bob2', check: (ok, warning) => warning()});
+            mon.addReadinessCheck({name: 'bob2', check: (ok, warning, critical) => critical()});
+            mon.addReadinessCheck({name: 'bob2', check: (ok, warning, critical, unknown) => unknown()});
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.sendStatus.calledWith(500));
+                    done();
+                });
         });
     });
 
-    it('should return 200 if all checks succeed', (done) => {
-      mon.addReadinessCheck({ name: 'bob', check: (ok) => ok() });
-      mon.addReadinessCheck({ name: 'bob2', check: (ok) => ok() });
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.sendStatus.calledWith(200));
-          done();
+    describe('/checkz endpoint', () => {
+        beforeEach(() => {
+            req.path = '/checkz';
+        });
+
+        it('should return 404 by default', (done) => {
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.sendStatus.calledWith(404));
+                    done();
+                });
+        });
+
+        it('can be configured to do checks that succeed', (done) => {
+            mon.addCheck({name: 'bob0', check: (ok) => ok()});
+            mon.addCheck({name: 'bob1', check: (ok) => ok()});
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.send.calledWith('bob0 0\nbob1 0\n'));
+                    done();
+                });
+        });
+
+        it('can be configured to do checks that warn and fail', (done) => {
+            mon.addCheck({name: 'bob0', check: (ok) => ok()});
+            mon.addCheck({name: 'bob1', check: (ok, warning) => warning()});
+            mon.addCheck({name: 'bob2', check: (ok, warning, critical) => critical()});
+            mon.addCheck({name: 'bob3', check: (ok, warning, critical, unknown) => unknown()});
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.send.calledWith('bob0 0\nbob1 1\nbob2 2\nbob3 3\n'));
+                    done();
+                });
         });
     });
 
-    it('should return 500 if any check fails', (done) => {
-      mon.addReadinessCheck({ name: 'bob', check: (ok) => ok() });
-      mon.addReadinessCheck({ name: 'bob2', check: (ok, warning) => warning() });
-      mon.addReadinessCheck({ name: 'bob2', check: (ok, warning, critical) => critical() });
-      mon.addReadinessCheck({ name: 'bob2', check: (ok, warning, critical, unknown) => unknown() });
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.sendStatus.calledWith(500));
-          done();
+    describe('/livez endpoint', () => {
+        beforeEach(() => {
+            req.path = '/livez';
         });
-    });
-  });
 
-  describe('/checkz endpoint', () => {
-    beforeEach(() => {
-      req.path = '/checkz';
-    });
-
-    it('should return 404 by default', (done) => {
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.sendStatus.calledWith(404));
-          done();
+        it('should return 200 by default', (done) => {
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.sendStatus.calledWith(200));
+                    done();
+                });
         });
-    });
 
-    it('can be configured to do checks that succeed', (done) => {
-      mon.addCheck({ name: 'bob0', check: (ok) => ok() });
-      mon.addCheck({ name: 'bob1', check: (ok) => ok() });
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.send.calledWith('bob0 0\nbob1 0\n'));
-          done();
+        it('can be configured to do checks that succeed', (done) => {
+            mon.addCheck({name: 'bob0', check: (ok) => ok()});
+            mon.addCheck({name: 'bob1', check: (ok) => ok()});
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.status.calledWith(200));
+                    assert(res.send.calledWith('bob0 0\nbob1 0\n'));
+                    done();
+                });
         });
-    });
 
-    it('can be configured to do checks that warn and fail', (done) => {
-      mon.addCheck({ name: 'bob0', check: (ok) => ok() });
-      mon.addCheck({ name: 'bob1', check: (ok, warning) => warning() });
-      mon.addCheck({ name: 'bob2', check: (ok, warning, critical) => critical() });
-      mon.addCheck({ name: 'bob3', check: (ok, warning, critical, unknown) => unknown() });
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.send.calledWith('bob0 0\nbob1 1\nbob2 2\nbob3 3\n'));
-          done();
-        });
-    });
-  });
-
-  describe('/livez endpoint', () => {
-    beforeEach(() => {
-      req.path = '/livez';
-    });
-
-    it('should return 200 by default', (done) => {
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.sendStatus.calledWith(200));
-          done();
+        it('can be configured to do checks that warn and fail', (done) => {
+            mon.addCheck({name: 'bob0', check: (ok) => ok()});
+            mon.addCheck({name: 'bob1', check: (ok, warning) => warning()});
+            mon.addCheck({name: 'bob2', check: (ok, warning, critical) => critical()});
+            mon.addCheck({name: 'bob3', check: (ok, warning, critical, unknown) => unknown()});
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.status.calledWith(500));
+                    assert(res.send.calledWith('bob0 0\nbob1 1\nbob2 2\nbob3 3\n'));
+                    done();
+                });
         });
     });
 
-    it('can be configured to do checks that succeed', (done) => {
-      mon.addCheck({ name: 'bob0', check: (ok) => ok() });
-      mon.addCheck({ name: 'bob1', check: (ok) => ok() });
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.status.calledWith(200));
-          assert(res.send.calledWith('bob0 0\nbob1 0\n'));
-          done();
+    describe('/metricz endpoint', () => {
+        beforeEach(() => {
+            req.path = '/metricz';
+        });
+
+        it('should return 404 by default', (done) => {
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.sendStatus.calledWith(404));
+                    done();
+                });
+        });
+
+        it('should return some generic metrics if passed in', (done) => {
+            mon.addMetrics(() => 'some string basically');
+            const mid = mon.getMiddleware();
+            mid(req, res, next)
+                .then(() => {
+                    assert(res.send.calledWith('some string basically'));
+                    done();
+                });
         });
     });
 
-    it('can be configured to do checks that warn and fail', (done) => {
-      mon.addCheck({ name: 'bob0', check: (ok) => ok() });
-      mon.addCheck({ name: 'bob1', check: (ok, warning) => warning() });
-      mon.addCheck({ name: 'bob2', check: (ok, warning, critical) => critical() });
-      mon.addCheck({ name: 'bob3', check: (ok, warning, critical, unknown) => unknown() });
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.status.calledWith(500));
-          assert(res.send.calledWith('bob0 0\nbob1 1\nbob2 2\nbob3 3\n'));
-          done();
+    describe('Content-Type header', () => {
+        it('is not set if no paths match', () => {
+            req.path = '/nothing-interesting';
+            const mid = mon.getMiddleware();
+            mid(req, res, next);
+            assert.equal(res.set.callCount, 0);
+        });
+
+        it('is set for /metricz', () => {
+            req.path = '/metricz';
+            const mid = mon.getMiddleware();
+            mid(req, res, next);
+            assert.equal(res.set.callCount, 1);
+        });
+
+        it('is set for /checkz', () => {
+            req.path = '/checkz';
+            const mid = mon.getMiddleware();
+            mid(req, res, next);
+            assert.equal(res.set.callCount, 1);
+        });
+
+        it('is set for /livez', () => {
+            req.path = '/livez';
+            const mid = mon.getMiddleware();
+            mid(req, res, next);
+            assert.equal(res.set.callCount, 1);
+        });
+
+        it('is set for /healthz', () => {
+            req.path = '/healthz';
+            const mid = mon.getMiddleware();
+            mid(req, res, next);
+            assert.equal(res.set.callCount, 1);
         });
     });
-  });
-
-  describe('/metricz endpoint', () => {
-    beforeEach(() => {
-      req.path = '/metricz';
-    });
-
-    it('should return 404 by default', (done) => {
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.sendStatus.calledWith(404));
-          done();
-        });
-    });
-
-    it('should return some generic metrics if passed in', (done) => {
-      mon.addMetrics(() => 'some string basically');
-      const mid = mon.getMiddleware();
-      mid(req, res, next)
-        .then(() => {
-          assert(res.send.calledWith('some string basically'));
-          done();
-        });
-    });
-  });
-
-  describe('Content-Type header', () => {
-    it('is not set if no paths match', () => {
-      req.path = '/nothing-interesting';
-      const mid = mon.getMiddleware();
-      mid(req, res, next);
-      assert.equal(res.set.callCount, 0);
-    });
-
-    it('is set for /metricz', () => {
-      req.path = '/metricz';
-      const mid = mon.getMiddleware();
-      mid(req, res, next);
-      assert.equal(res.set.callCount, 1);
-    });
-
-    it('is set for /checkz', () => {
-      req.path = '/checkz';
-      const mid = mon.getMiddleware();
-      mid(req, res, next);
-      assert.equal(res.set.callCount, 1);
-    });
-
-    it('is set for /livez', () => {
-      req.path = '/livez';
-      const mid = mon.getMiddleware();
-      mid(req, res, next);
-      assert.equal(res.set.callCount, 1);
-    });
-
-    it('is set for /healthz', () => {
-      req.path = '/healthz';
-      const mid = mon.getMiddleware();
-      mid(req, res, next);
-      assert.equal(res.set.callCount, 1);
-    });
-  });
 });
